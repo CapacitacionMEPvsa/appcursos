0b0dbe0038469113df9318e548f029db22546763
CapacitacionMEPvsa <elizabethhdez2025@gmail.com>
2026-04-20 15:49:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ for b in bloques:
     })
 
     temp["categoria"] = b["categoria"]
-    temp["observaciones"] = df.iloc[:, b["inicio"] - 3]
+    temp["observaciones"] = df.iloc[:, b["inicio"] - 4]
 
     # ✅ CURSO VIENE DEL HEADER (fila 2)
     temp["curso"] = df.columns[b["inicio"]]
