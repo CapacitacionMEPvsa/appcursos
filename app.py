63b80e19f6b40b9f051a9924585036301ec0da59
CapacitacionMEPvsa <elizabethhdez2025@gmail.com>
2026-04-21 00:06:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ st.title("Consulta de Cursos")
 FILE = "BASE DE DATOS DE CURSOS DE CAPACITACION VSA.xlsx"
 
 df_raw = pd.read_excel(FILE, header=None)
+for col in df_raw.columns:
+    df_raw[col] = pd.to_datetime(df_raw[col], errors="ignore").apply(
+        lambda x: x.date() if hasattr(x, "date") else x
+    )
 
 # fila donde están los nombres de cursos
 fila_cursos = df_raw.iloc[1]
