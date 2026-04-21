f52810b6b40ee67e6ad2e4abdd1f9f051291eeda
CapacitacionMEPvsa <elizabethhdez2025@gmail.com>
2026-04-20 23:54:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ categorias = {
     "CURSOS TÉCNICOS": [(198, 262), (289, 312)],
     "CURSOS DE SEGURIDAD": [(32, 167)],
     "CURSOS EXTERNOS": [(5, 32), (319, 381)],
-    "CURSOS COMPLEMENTARIOS": [(170, 197), (264, 288)]
+    "CURSOS COMPLEMENTARIOS": [(168, 197), (263, 287)]
 }
 
 SALTO = 5 # columnas por curso
