4bcbf8ebf1c0df288abe8f2efd2bc240ee9a0974
CapacitacionMEPvsa <elizabethhdez2025@gmail.com>
2026-04-20 23:10:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ st.markdown(f"## 👤 {nombre}")
 categorias = {
     "CURSOS TÉCNICOS": (198, 262),
     "CURSOS DE SEGURIDAD": (32, 167),
-    "CURSOS EXTERNOS": (4, 31),
+    "CURSOS EXTERNOS": (5, 32),
     "CURSOS COMPLEMENTARIOS": (266, df.shape[1])
 }
 
