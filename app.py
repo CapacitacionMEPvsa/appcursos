d300c26c6c9ba4899ff73bc765c38cc866f40dfa
CapacitacionMEPvsa <elizabethhdez2025@gmail.com>
2026-04-22 10:26:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -78,8 +78,8 @@ if empleado_df.empty:
     st.stop()
 
 fila = empleado_df.iloc[0]
-nombre = base_fila[COL_NOMBRE]
-proceso = base_fila.iloc[3]
+nombre = fila[COL_NOMBRE]
+proceso = fila.iloc[3]
 
 col1, col2 = st.columns([6,1])
 
