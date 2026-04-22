daf683a4af7d66052cfd994160c28bb0e607b2f2
CapacitacionMEPvsa <elizabethhdez2025@gmail.com>
2026-04-21 20:56:15 -0600

    Update app.py

--- app.py
+++ app.py
@@ -252,7 +252,7 @@ def generar_pdf(nombre, datos_dict, nomina="N/A", proceso="N/A"):
 
                 pdf.ln()
 
-                pdf.ln(0)
+            pdf.ln(6)
         else:
             # 👉 lo técnico lo dejas como ya lo tienes
             pdf.set_font("Helvetica", "B", 12)
