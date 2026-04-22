2b2d0af3dbdcd7c4db24f3c07113dd680aaac453
CapacitacionMEPvsa <elizabethhdez2025@gmail.com>
2026-04-21 23:46:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ with col2:
         st.rerun()
 
 st.markdown(f"## 👤 {nombre}")
-filtro_activo = st.toggle("🚀 Por Vencer / Vencido")
+filtro_activo = st.toggle("🚀 Por Vencer / Vencida")
 
 st.markdown("---")
 
