31661ce2dc906c5c4c1d2311c29ba4a0efdb6c4d
CapacitacionMEPvsa <elizabethhdez2025@gmail.com>
2026-04-22 13:01:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ def obtener_cursos(rangos):
 
             # 🔹 OBSERVACIONES (NO SE TOCA)
             try:
-                observaciones = fila.iloc[col + 6]
+                observaciones = fila.iloc[col + 7]
             except:
                 pass
 
