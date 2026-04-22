4fe8db34f26678bb8e285f01485cb2f83c57f15f
CapacitacionMEPvsa <elizabethhdez2025@gmail.com>
2026-04-21 20:42:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -196,14 +196,14 @@ def generar_pdf(nombre, datos_dict, nomina="N/A", proceso="N/A"):
     # =========================
     # 📊 TABLAS
     # =========================
-    GRIS_CLARO = (153, 255, 0)
+    GRIS = (200, 200, 200)
     for categoria, df in datos_dict.items():
 
         # 👉 SOLO aplicar nuevo formato desde SEGURIDAD en adelante
         if categoria in ["CURSOS DE SEGURIDAD", "CURSOS EXTERNOS", "CURSOS COMPLEMENTARIOS"]:
 
-            # ⚪ BARRA GRIS CLARO (título de sección)
-            pdf.set_fill_color(*GRIS_CLARO)
+            # ⚪ BARRA GRIS (título de sección)
+            pdf.set_fill_color(*GRIS)
             pdf.set_text_color(0, 0, 0)
             pdf.set_font("Helvetica", "B", 11)
             pdf.cell(0, 8, categoria, ln=True, fill=True)
