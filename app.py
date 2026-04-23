fee109f8c514b1d4dffc7ff7dc8a842f92348ab9
CapacitacionMEPvsa <elizabethhdez2025@gmail.com>
2026-04-23 14:33:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -254,7 +254,7 @@ def generar_pdf(nombre, datos_dict, nomina="N/A", proceso="N/A"):
     pdf.set_font("Helvetica", "", 8)
     pdf.set_xy(50, 27)
     pdf.cell(180, 5,
-        "Elaboración: 10-may-21               Revisión: 20-abr-26                 Emisión: 22-abr-26             Versión: 02           Idioma: ES",
+        "Elaboración: 10-may-21                                      Revisión: 20-abr-26                                    Emisión: 22-abr-26                           Versión: 02                      Idioma: ES",
         align="C"
     )
     # =========================
