047ae2cf52a9bade91c9ab79c490b34a2a4c81ad
CapacitacionMEPvsa <elizabethhdez2025@gmail.com>
2026-04-23 23:59:50 -0600

    Update app.py

--- app.py
+++ app.py
@@ -394,18 +394,11 @@ def generar_pdf(nombre, datos_dict, nomina="N/A", proceso="N/A"):
                 else:
                     pdf.set_fill_color(255, 255, 255)
 
-            for i, col in enumerate(columnas):
-
-                valor = str(row.get(col, ""))[:100]
-                valor = valor.encode("latin-1", "ignore").decode("latin-1")
+                for i, col in enumerate(columnas):
+                    valor = str(row.get(col, ""))[:100]
+                    valor = valor.encode("latin-1", "ignore").decode("latin-1")
                 
-                # 🎯 CENTRAR SOLO VENCIMIENTO Y ESTATUS
-                if col in ["Vencimiento", "Estatus"]:
-                    align = "C"
-                else:
-                    align = "L"
-
-                pdf.cell(col_widths[i], 6, valor, border=1, fill=True, align=align)
+                    pdf.cell(col_widths[i], 6, valor, border=1, fill=True)
 
                 pdf.ln()
 
