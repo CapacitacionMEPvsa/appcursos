ce3f2b996f42f5d945da9bd45b3d021b8af48a5c
CapacitacionMEPvsa <elizabethhdez2025@gmail.com>
2026-05-03 19:48:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -543,5 +543,20 @@ for categoria, cursos_base in categorias.items():
     # -------------------------
     # MOSTRAR
     # -------------------------
+    
     st.markdown(f"## 📂 {categoria}")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        if categoria == "CURSOS TÉCNICOS":
+            st.link_button("🎓 Ir a Cursos Técnicos", "https://capacitacion-online-3.netlify.app/")
+
+    with col2:
+        if categoria == "CURSOS COMPLEMENTARIOS":
+            st.link_button("📘 Ir a Cursos Complementarios", "https://capacitacion-en-linea.netlify.app/")
+
+    with col3:
+        if categoria == "CURSOS DE SEGURIDAD":
+            st.link_button("🦺 Ir a Cursos de Seguridad", "https://capacitacion-online-2.netlify.app/")
     st.dataframe(df_cat, use_container_width=True)
